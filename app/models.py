c7accf4ad90eeeb6425c8bb7c2c381bd4a680c87
James Alexander <james@jamesralexander.com>
2014-01-04 09:34:51 -0500

    - Adding logic to run initial import from old SQL database - Changing the FillUp object's properties to be prefixed with underscores - Adding requirements.txt file

--- app/models.py
+++ app/models.py
@@ -1,21 +1,22 @@
 from decimal import Decimal
+from time import strptime
 
 class FillUp(object):
     def __init__(self, date = "", miles = 0, price = 0, gallons = 0, latitude = 0, longitude = 0):
-        self._date      = date
-        self._miles     = miles
-        self._price     = price    
-        self._gallons   = gallons 
-        self._latitude  = latitude 
+        self._date = date
+        self._miles = miles
+        self._price = price    
+        self._gallons = gallons 
+        self._latitude = latitude 
         self._longitude = longitude
 
     @property
     def mileage(self):
-        return Decimal(self.gallons) / Decimal(self.miles)
+        return round(self._miles / self._gallons, 2)
 
     @property
     def date(self):
-        return self._date.date()
+        return self._date
 
     @property
     def miles(self):
@@ -31,8 +32,11 @@ class FillUp(object):
     
     @property
     def latitude(self):
-        return self._latitude
+        return round(self._latitude, 2)
 
     @property
     def longitude(self):
-        return self._longitude
+        return round(self._longitude, 2)
+
+    def __repr__(self):
+        return "{0} - {1} - {2} - {3}".format(self._date, self._miles, self._price, self._gallons)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app, db
 from flask import render_template, request, flash, jsonify, url_for
 from datetime import datetime
+from pytz import timezone
 from models import FillUp
-import os
+import os, csv
+from time import strptime
+from decimal import Decimal
 
 @app.route('/')
 def index():
@@ -18,16 +21,14 @@ def getFillUpsTree():
 @app.route('/submitMileage', methods = ['POST'])
 def submitMileage():
     tree = getFillUpsTree()
-    fillUp = FillUp(datetime.now(), request.form["miles"], 
+    fillUp = FillUp(datetime.utcnow(), request.form["miles"], 
         request.form["price"], request.form["gallons"], request.form["latitude"],
         request.form["longitude"])
     tree[fillUp.date] = fillUp
 
     flash("Fillup Saved")
-    result = {}
-    result["success"] = True
-    result["message"] = ""
-    return jsonify(result)
+    flash("Fillup Cost: {0}".format(fillUp.price * fillUp.gallons))
+    flash("Mileage: {0}".format(fillUp.miles * fillUp.price))
 
 @app.route('/recentHistory')
 def recentHistory():
@@ -42,6 +43,58 @@ def recentHistory():
 
     return render_template("recentHistory.html", recentHistory = recentHistory)
 
+@app.route('/import')
+def importRecords():
+    records = []
+    csvImportFile = app.config['MILEAGE_IMPORT_CSV']
+
+    with open(csvImportFile) as f:
+        data = csv.reader(f)
+        fields = data.next()
+
+        fillUpTree = getFillUpsTree()
+
+        for row in data:
+            if len(row) == 0:
+                break
+            items = zip(fields, row)
+            item = {}
+            for (name, value) in items:
+                item[name] = value
+            fillUp = FillUp(
+                date = convertToUtcDate(strptime(item["Date"], "%Y-%m-%d %H:%M:%S"))
+                , miles = Decimal(item["FillUpMileage"])
+                , gallons = Decimal(item["FillUpGallons"])
+                , price = Decimal(item["FillUpCostPerGallon"])
+                , latitude = Decimal(item["Latitude"])
+                , longitude = Decimal(item["Longitude"]))
+
+            print("Date: {0}".format(fillUp.date))
+            fillUpTree[fillUp.date] = fillUp
+            records.append(fillUp)
+
+    return render_template("import_results.html", records = records)
+
+def convertToUtcDate(time_struct):
+    estzone = timezone("US/Eastern")
+    utczone = timezone("UTC")
+    if time_struct.tm_hour == 0 and time_struct.tm_min == 0 and time_struct.tm_sec == 0:
+        estDateTime = datetime(year = time_struct.tm_year, 
+            month = time_struct.tm_mon, 
+            day = time_struct.tm_mday, 
+            tzinfo = estzone)
+    else:
+        estDateTime = datetime(year = time_struct.tm_year, 
+            month = time_struct.tm_mon, 
+            day = time_struct.tm_mday, 
+            hour =  time_struct.tm_hour,
+            minute = time_struct.tm_min,
+            second = time_struct.tm_sec,
+            tzinfo = estzone)
+
+    utcDateTime = estDateTime.astimezone(utczone)
+    return utcDateTime 
+
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
@@ -54,5 +107,3 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
 
     return url_for(endpoint, **values)
-
-        
